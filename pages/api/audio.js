7e91e5c25d519e875983d399512652d8722ac24c
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2022-03-02 22:44:17 +0800

    Update audio.js

--- pages/api/audio.js
+++ pages/api/audio.js
@@ -1,6 +1,28 @@
 import aws from 'aws-sdk'
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD', 'POST'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
 
 export default async function get_track(req, res) {
+    await runMiddleware(req, res, cors);
+
     if (req.method === 'GET') {
         res.status(200).json('API route for fetching audio');
         return
