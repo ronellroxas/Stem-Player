d7fde31223de134663b5bd8b67db8ccfb9e46f1e
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2022-03-02 20:41:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    basePath: '/Stem-Player',
+    assetPrefix: '/Stem-Player'
 }
 
 module.exports = nextConfig
